f0f54ae24e4572f3e655b86fd0d83a14784297bd
Louiza Boudjemai <lboudjem@z3r4p1.42lyon.fr>
2026-01-06 14:14:12 +0100

    Clean + Add error cases

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,6 +7,7 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.List (elemIndex)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
+import Errors
 import Machine
 import Print
 import Tape
@@ -60,57 +61,6 @@ convertTransitions machineJson =
         )
         (zip [0 ..] statesList)
 
--- check all error cases in the JSON file
-checkParsingErrors :: MachineJSON -> Either String ()
-checkParsingErrors machineJson = do
-  -- check if the blanck symbol is in the alphabet
-  let blankSymbol = Types.blank machineJson
-      alphabetList = Types.alphabet machineJson
-  if blankSymbol `notElem` alphabetList
-    then Left $ "Error: blank character '" ++ blankSymbol ++ "' must be part of the alphabet"
-    else Right ()
-
-  -- check if the initial state exists
-  let initialState = Types.initial machineJson
-      statesList = Types.states machineJson
-  if initialState `notElem` statesList
-    then Left $ "Error: initial state '" ++ initialState ++ "' is not in the states list"
-    else Right ()
-
-  -- check if final states exist
-  let finalsList = Types.finals machineJson
-      invalidFinals = filter (`notElem` statesList) finalsList
-  if not (null invalidFinals)
-    then Left $ "Error: final state(s) " ++ show invalidFinals ++ " are not in the states list"
-    else Right ()
-
-  -- check if states in transitions exist
-  let transMap = Types.transitions machineJson
-      transStates = Map.keys transMap
-      invalidTransStates = filter (`notElem` statesList) transStates
-  if not (null invalidTransStates)
-    then Left $ "Error: transition state(s) " ++ show invalidTransStates ++ " are not in the states list"
-    else Right ()
-
-  -- check if to_state in each transition exists
-  let allTransitions = concat $ Map.elems transMap
-      toStates = map Types.to_state allTransitions
-      invalidToStates = filter (`notElem` statesList) toStates
-  if not (null invalidToStates)
-    then Left $ "Error: target state(s) " ++ show invalidToStates ++ " in transitions are not in the states list"
-    else Right ()
-
-  -- check if symbols in transitions exist in alphabet
-  let readSymbols = map Types.read allTransitions
-      writeSymbols = map Types.write allTransitions
-      allSymbols = readSymbols ++ writeSymbols
-      invalidSymbols = filter (`notElem` alphabetList) allSymbols
-  if not (null invalidSymbols)
-    then Left $ "Error: symbol(s) " ++ show invalidSymbols ++ " in transitions are not in the alphabet"
-    else Right ()
-
-  Right ()
-
 createMachineFromJSON :: FilePath -> String -> IO Machine
 createMachineFromJSON filePath input = do
   decoded <- parseMachineJSON filePath
@@ -142,8 +92,3 @@ createMachineFromJSON filePath input = do
 parser :: FilePath -> String -> IO ()
 parser filePath input = do
   printMachineJson filePath
-
--- ajout d'une fonction de verification d'erreur dans le parsing:
--- - verif que le blank est dans l'alphabet
--- - verif que les etats dans les transitions existent
--- - verif que les symboles dans les transitions existent dans l'alphabet
